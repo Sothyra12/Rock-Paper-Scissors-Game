6be6edc85666df32b24de17d8f2fb2ec032de58c
Sothyra12 <schan256@my.centennialcollege.ca>
2024-09-06 23:34:31 -0400

    created randomIndex variable representing the element of the array using math.floor() and math.random()

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 /*
 ** Step 1
 The first step is to build out the function that will generate a random choice for the computer.
-The getRandomComputerResult function will be used to get the computer's choice. Inside that function, you should see an options array with "Rock", "Paper", and "Scissors".
+The getRandomComputerResult function will be used to get the computer's choice.
+Inside that function, you should see an options array with "Rock", "Paper", and "Scissors".
 Your task is to complete the getRandomComputerResult function so that it returns a random option from the options array.
 Tips
 You can use Math.random() and Math.floor() to help you get a random whole number. This will represent the index number for the options array.
 You can use the random index to access the option from the options array.
 
+** Step 2
+In the game, there will be multiple rounds. The first to reach three points wins the game.
+In this step, you will focus on determining if the player has won the round.
+Complete the hasPlayerWonTheRound function. This function has two parameters:
+player and computer. The function should return true if the player has won the round,
+and false if the player has lost or tied the round.
+Here are the criteria for the player to win a round:
+If the player chooses "Rock" and the computer chooses "Scissors"
+If the player chooses "Scissors" and the computer chooses "Paper"
+If the player chooses "Paper" and the computer chooses "Rock"
+A few function calls have been provided for you to test your function.
+
 */
+function getRandomComputerResult() {
+    const options = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+  }
+console.log(getRandomComputerResult());
