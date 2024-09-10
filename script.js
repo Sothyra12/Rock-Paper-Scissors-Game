41d999cd66ddf8206eaed7bab54e7c09ca84334a
Sothyra12 <schan256@my.centennialcollege.ca>
2024-09-10 00:22:27 -0400

    added nested if-else statement to check and return the respective messages to winners or ties

--- script.js
+++ script.js
@@ -57,7 +57,15 @@ function hasPlayerWonTheRound(player, computer) {
 
 function getRoundResults(userOption) {
   const computerResult = getRandomComputerResult();
-
+  if (hasPlayerWonTheRound(userOption, computerResult)) {
+    playerScore++;
+    return `Player wins! ${userOption} beats ${computerResult}`;
+  } else if (computerResult === userOption) {
+    return `It's a tie! Both chose ${userOption}`;
+  } else {
+    computerScore++;
+    return `Computer wins! ${computerResult} beats ${userOption}`;
+  }
 }
 
 console.log(getRoundResults("Rock"));
